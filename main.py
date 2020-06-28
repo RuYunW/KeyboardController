72a67fa0ff296a368602b29d8b2e25db03acbd18
Ash <ruyunw@163.com>
2020-06-28 20:31:13 +0800

    GUI

--- main.py
+++ main.py
@@ -1,47 +1,12 @@
-import speech_recognition as sr
-import logging
-from pykeyboard import *
-import time
-from funcs import run_all_time
-from wind import MyApp
-
-app=MyApp()
-logging.basicConfig(level=logging.DEBUG)
-k = PyKeyboard()  # ?
-
-while True:
-    r = sr.Recognizer()
-    # 麦克风
-    mic = sr.Microphone()
-
-    logging.info('录音中...')
-    with mic as source:
-        r.adjust_for_ambient_noise(source)
-        audio = r.listen(source)
-    logging.info('录音结束，识别中...')
-    test = r.recognize_google(audio, language='zh-CN', show_all=True)
-    print(test)
-    if len(test)>0:  # 如果检测到有语音
-        control = test['alternative'][0]['transcript']
-        print(control)
-        if '向左转' in control:
-            k.tap_key('a')
-        elif '向右转' in control:
-            k.tap_key('d')
-        elif '向后转' in control:
-            k.tap_key('s')
-        elif '前进' in control:
-            k.press_key('w')  # 按住 W 键两秒
-            time.sleep(2)
-            k.release_key('w')
-        elif '一直跑' in control:
-            k.press_key('w')
-        else:
-            pass
-    else:
-        continue
-
-    logging.info('end')  # 退出登录
+from wind import *
+import wx
 
 
+app = wx.App(False)
+frame = MainWindow(None, 'hello')
 app.MainLoop()
+
+
+
+
+
--- wind.py
+++ wind.py
@@ -1,7 +1,11 @@
 import wx
 
-class MyApp(wx.App):
-    def OnInit(self):  #初始化接口，子类覆盖父类的方法
-        frame=wx.Frame(parent=None,title="hello wxpython")  #新建框架
-        frame.Show()  #显示
-        return True
+
+class MainWindow(wx.Frame):
+    """We simply derive a new class of Frame."""
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(200, 200))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        for i in range(10):
+            self.control.AppendText('hi\n')
+        self.Show(True)
