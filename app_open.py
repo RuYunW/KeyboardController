a53ed917f07f7e279f75fe6a86253943efcda501
Ash <ruyunw@163.com>
2020-06-29 20:28:33 +0800

    open app

---
+++ app_open.py
@@ -0,0 +1,5 @@
+import os
+
+
+def app_open(app_dir):
+    os.startfile(app_dir)
--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from wind import *
 import wx
 import _thread
 from kb_ctrler import keyboard_controller
+from app_open import app_open
 
 
+app_dir = r'G:\Unity3D项目\My1stGame.exe'
 app = wx.App(False)
 frame = MainWindow(None, 'Message Table')
 try:
@@ -11,6 +13,7 @@ try:
 except:
     print("Error: 无法启动线程")
 
+app_open(app_dir)
 app.MainLoop()
 
 
